9b439f6977ce5d5cfa283a9987e87cba95d5fdc3
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-08-11 13:42:16 -0300

    WIP - Aula 4 completa;

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 
 
 @Component({
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-busca.component.scss']
 })
 export class FormBuscaComponent {
+  constructor(public dialog: MatDialog) {
 
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent);
+  }
 }
