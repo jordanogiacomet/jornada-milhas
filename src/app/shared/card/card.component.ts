88d2456d0eaa07a798dfd74865e408958ec57d94
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-08-11 11:03:59 -0300

    WIP - Aula 2 completa;

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
 
+// Uma classe condicional, o componente que usar essa variante vai usar primary, mas podemos usar o secondary
+export class CardComponent {
+  @Input() variant: 'primary' | 'secondary' = 'primary';
 }
