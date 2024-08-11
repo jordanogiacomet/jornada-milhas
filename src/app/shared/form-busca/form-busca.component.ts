3b94bb816c92dac8daca18838a391541e87006b2
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-08-11 12:27:48 -0300

    WIP - Aula 3 completa;

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent {
+
+}
